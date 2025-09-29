c698135ad0c3cc220f6586ee7a15b2e5f0b87882
SoliBlack <SolitaireBlack21@gmail.com>
2025-09-28 22:37:36 -0400

    Fixed the logo issue on the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+import nugget from '../assets/TheDailyNuggetRaw.png'
+import appLogo from '../assets/AppStoreImage.svg'
+
 export default function Header() {
     return (
         <>
@@ -5,9 +8,9 @@ export default function Header() {
                 <div className="container px-4 text-center">
                     <h1 className="fw-bolder">Welcome to The Daily Nugget</h1>
                     <p className="lead">Daily Nuggets of Quotes • Facts • Affirmations • Jokes.</p>
-                    <img className="mb-4 img-fluid mx-auto d-block w-25 rounded" src="./src/assets/TheDailyNuggetRaw.png" alt="Chick E. Nugget" />
+                    <img className="mb-4 img-fluid mx-auto d-block w-25 rounded" src={nugget} alt="Chick E. Nugget" />
                     <a className="" href="https://apps.apple.com/us/app/thedailynugget/id6745912191">
-                        <img className="" src="./src/assets/AppStoreImage.svg" alt="App Store Icon" />
+                        <img className="" src={appLogo} alt="App Store Icon" />
                     </a>
                 </div>
             </header>
